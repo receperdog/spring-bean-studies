d71f76f31d8c19ec1016b3997f74b16600b44410
receptayyiperdogan <receptayyiperdogan8@gmail.com>
2023-09-16 00:20:34 +0300

    Test Component, PostConstruct & PreDestroy

--- src/main/java/com/example/springbeanstudies/SpringBeanStudiesApplication.java
+++ src/main/java/com/example/springbeanstudies/SpringBeanStudiesApplication.java
@@ -1,52 +1,68 @@
 package com.example.springbeanstudies;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @SpringBootApplication
-public class SpringBeanStudiesApplication {
+public class SpringBeanStudiesApplication implements CommandLineRunner {
 
+    @Autowired
+    private TestPasswordGenerator testPasswordGenerator;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBeanStudiesApplication.class, args);
     }
 
-    @Bean
-    public Customer customer(@Qualifier("test") String address){
-        return new Customer("Recep", address);
-    }
-
-    @Bean
-    public Customer temporary(@Autowired Customer customer){
-        System.out.println(customer);
-        return customer;
-    }
-
-}
-
-@Configuration
-class Address {
-    @Bean("test")
-    public String address(){
-        return "Elbistan";
+    @Override
+    public void run(String... args) throws Exception {
+        testPasswordGenerator.test();
     }
 }
 
-@Configuration
-class TestAdddress{
-    @Bean("anotherTest")
-    public String address(){
-        return "Elbistan";
+@Component
+class PasswordGenerator {
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyz";
+    private static final Random random = new Random();
+
+    public String generate(int length) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            result.append(CHARACTERS.charAt(index));
+        }
+
+        return result.toString();
+    }
+    @PostConstruct
+    public void testPostConstruct(){
+        System.out.println("AFTER construction this method should be run");
+    }
+
+    @PreDestroy
+    public void testPreDestroy(){
+        System.out.println("Before Destruction this method should be executed");
     }
 }
 
-@Data
-class Customer{
-    private final String name;
-    private final String address;
+@Service
+class TestPasswordGenerator{
+    @Autowired
+    private PasswordGenerator passwordGenerator;
+
+    public void test(){
+        System.out.println(passwordGenerator.generate(10));
+    }
 }
